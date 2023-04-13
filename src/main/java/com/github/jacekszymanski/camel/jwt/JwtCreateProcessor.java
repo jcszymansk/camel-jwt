d697b7f6404271b618d68d06d2717c395945d9c6
jszymanski <jacek.p.szymanski@gmail.com>
2023-04-14 00:00:00 +0200

    refactor: move resolveKey() to new KeyUtil

--- src/main/java/com/github/jacekszymanski/camel/jwt/JwtCreateProcessor.java
+++ src/main/java/com/github/jacekszymanski/camel/jwt/JwtCreateProcessor.java
@@ -1,20 +1,11 @@
 package com.github.jacekszymanski.camel.jwt;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.camel.CamelContext;
-import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.apache.camel.support.ResourceHelper;
 import org.jose4j.jwa.AlgorithmConstraints;
 import org.jose4j.jws.JsonWebSignature;
 import org.jose4j.jwt.JwtClaims;
-import org.jose4j.keys.HmacKey;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.Key;
 
 @RequiredArgsConstructor
 public class JwtCreateProcessor implements Processor {
@@ -33,34 +24,10 @@ public class JwtCreateProcessor implements Processor {
     }
     signature.setHeader("typ", "JWT");
     signature.setPayload(claims.toJson());
-    signature.setKey(resolveKey(exchange));
+    signature.setKey(KeyUtil.resolveKey(endpoint, exchange));
 
     exchange.getIn().setBody(signature.getCompactSerialization());
 
   }
 
-  public Key resolveKey(final Exchange exchange) throws IOException {
-    final CamelContext ctx = endpoint.getCamelContext();
-
-    // a key *must* be provided as a resource path, providing bytes/whatever in a header or uri is
-    // not supported (it's a security nightmare)
-    final String privateKeyLocation =
-        exchange.getIn().getHeader(JwtConstants.JWT_PRIVATE_KEY_LOCATION, endpoint.getPrivateKeyLocation(), String.class);
-
-    if (privateKeyLocation == null) {
-      if (!endpoint.getAlgorithm().equals(JwtAlgorithm.none)) {
-        throw new IllegalArgumentException("No key location provided");
-      }
-
-      return null;
-    }
-
-    // TODO: cache the key
-    try (InputStream keyInputStream = ResourceHelper.resolveMandatoryResourceAsInputStream(ctx, privateKeyLocation)) {
-      final ByteArrayOutputStream keyBytes = new ByteArrayOutputStream();
-      keyBytes.writeBytes(keyInputStream.readAllBytes());
-
-      return new HmacKey(keyBytes.toByteArray());
-    }
-  }
 }
---
+++ src/main/java/com/github/jacekszymanski/camel/jwt/KeyUtil.java
@@ -0,0 +1,38 @@
+package com.github.jacekszymanski.camel.jwt;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.support.ResourceHelper;
+import org.jose4j.keys.HmacKey;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.Key;
+
+public class KeyUtil {
+  public static Key resolveKey(final JwtEndpoint endpoint, final Exchange exchange) throws IOException {
+    final CamelContext ctx = endpoint.getCamelContext();
+
+    // a key *must* be provided as a resource path, providing bytes/whatever in a header or uri is
+    // not supported (it's a security nightmare)
+    final String privateKeyLocation =
+        exchange.getIn().getHeader(JwtConstants.JWT_PRIVATE_KEY_LOCATION, endpoint.getPrivateKeyLocation(), String.class);
+
+    if (privateKeyLocation == null) {
+      if (!endpoint.getAlgorithm().equals(JwtAlgorithm.none)) {
+        throw new IllegalArgumentException("No key location provided");
+      }
+
+      return null;
+    }
+
+    // TODO: cache the key
+    try (InputStream keyInputStream = ResourceHelper.resolveMandatoryResourceAsInputStream(ctx, privateKeyLocation)) {
+      final ByteArrayOutputStream keyBytes = new ByteArrayOutputStream();
+      keyBytes.writeBytes(keyInputStream.readAllBytes());
+
+      return new HmacKey(keyBytes.toByteArray());
+    }
+  }
+}
