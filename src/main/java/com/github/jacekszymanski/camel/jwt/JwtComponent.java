05fa9064f537cce1dc821adcc2de0cbbbd805f43
jszymanski <jacek.p.szymanski@gmail.com>
2023-04-15 00:00:00 +0200

    feat: support key loc in component

--- src/main/java/com/github/jacekszymanski/camel/jwt/JwtComponent.java
+++ src/main/java/com/github/jacekszymanski/camel/jwt/JwtComponent.java
@@ -2,16 +2,28 @@ package com.github.jacekszymanski.camel.jwt;
 
 import java.util.Map;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.camel.Endpoint;
 
+import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
 
 @Component("jwt")
 public class JwtComponent extends DefaultComponent {
 
+  @Metadata(description = "The location of the private key file")
+  @Getter @Setter
+  private String privateKeyLocation;
+
   protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
     JwtEndpoint endpoint = new JwtEndpoint(uri, this);
+
+    if (privateKeyLocation != null) {
+      endpoint.setPrivateKeyLocation(privateKeyLocation);
+    }
+
     final String[] parts = remaining.split(":");
     if (parts.length != 2) {
       throw new IllegalArgumentException("Invalid endpoint uri: " + uri);
