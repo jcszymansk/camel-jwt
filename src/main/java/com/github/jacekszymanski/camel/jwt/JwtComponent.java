80db953e2c82f39b68f376ee5552148ecb2d13ed
jszymanski <jacek.p.szymanski@gmail.com>
2023-04-15 00:00:00 +0200

    refactor: import Component annotation

--- src/main/java/com/github/jacekszymanski/camel/jwt/JwtComponent.java
+++ src/main/java/com/github/jacekszymanski/camel/jwt/JwtComponent.java
@@ -4,9 +4,10 @@ import java.util.Map;
 
 import org.apache.camel.Endpoint;
 
+import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
 
-@org.apache.camel.spi.annotations.Component("jwt")
+@Component("jwt")
 public class JwtComponent extends DefaultComponent {
 
   protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
